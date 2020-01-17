f941b58a607fcdce4028f7dee1225a3407607136
mazino ukah <ewomaukah@yahoo.com>
2020-01-17 17:52:06 +0100

    Updates

---
+++ precache-manifest.e970a5490bab218489978777194a8e40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb068357cc92b640fb86564e27ef210c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f3189c6132d5fe1aea4b",
+    "url": "/monsters-rolodex/static/css/main.b9b7066b.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3189c6132d5fe1aea4b",
+    "url": "/monsters-rolodex/static/js/main.e86d7ad0.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
